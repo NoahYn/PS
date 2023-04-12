2693db1cfe6fca8940d6168e7f5a80d481a339a1
noah <101003842+NoahYn@users.noreply.github.com>
2023-04-12 18:24:45 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+   int n1, n2;
+    
+   scanf("%d %d", &n1, &n2);
+    
+    if (n1 > n2)
+        printf(">");
+    else if(n1 < n2)
+        printf("<");
+    else
+       printf("==");
+    
+    return 0;
+}
