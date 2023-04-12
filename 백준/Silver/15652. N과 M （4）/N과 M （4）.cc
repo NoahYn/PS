ce43961d8133f227dc7c5e2db14116e63802c49f
noah <101003842+NoahYn@users.noreply.github.com>
2023-04-12 18:39:11 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int sel[10];
+bool isused[10];
+
+void print(int m2) {
+	if (m2 == m) {
+		for (int i = 0; i < m; i++) {
+			cout << sel[i] << ' ';
+		}
+		cout << '\n';
+		return ;
+	}
+	for (int i = sel[m2-1]; i <= n; i++) {
+//		if (isused[i] == false) {
+			sel[m2] = i;
+//			isused[i] = true;
+			print(m2+1); 
+//			isused[i] = false;
+//		}
+	}
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i) {
+		sel[0] = i;
+//		isused[i] = true;
+		print(1);
+//		isused[i] = false;
+	}
+}
