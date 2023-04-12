ee4571dd67282579eec0cc9163e16c9f2fd90f88
noah <101003842+NoahYn@users.noreply.github.com>
2023-04-12 18:36:55 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	int ans = 1;
+	for (int i = 1; i <= k; i++) {
+		ans *= n--;
+		ans /= i;
+	}
+	cout << ans;
+}
