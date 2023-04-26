82539940b24622028c1aa4b2a0934e0fd935d87a
noah <101003842+NoahYn@users.noreply.github.com>
2023-04-26 13:26:57 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10000];
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	next_permutation(arr, arr+n);
+	for (int i = 1; i < n; i++) {
+		if (arr[i] < arr[i-1]) {
+			for (int i = 0; i < n; i++) cout << arr[i] << ' ';
+			return 0;
+		} 
+	}
+	cout << -1;
+}
