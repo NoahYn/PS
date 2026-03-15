ab7f1628ddb6816a4ea12e0003ab8d309a8ba4c0
Sungmin Yoon <101003842+NoahYn@users.noreply.github.com>
2026-03-15 17:01:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 127.72 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    stack = [s[0]]
+    for i in range(1, len(s)) :
+        if stack and stack[-1] == s[i] :
+            stack.pop()
+        else :
+            stack.append(s[i])
+    
+    return 0 if stack else 1
+        
