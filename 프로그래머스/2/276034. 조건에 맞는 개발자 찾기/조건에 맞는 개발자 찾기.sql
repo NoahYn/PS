5bdd6174253e67f39404fe64d30ff7be5b838fa2
Sungmin Yoon <101003842+NoahYn@users.noreply.github.com>
2026-03-15 17:28:57 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+
+
+SELECT ID,	EMAIL,	FIRST_NAME,	LAST_NAME from DEVELOPERS
+WHERE SKILL_CODE & (SELECT CODE FROM SKILLCODES where name = 'Python') or SKILL_CODE & (SELECT CODE FROM SKILLCODES where name = 'C#')
+order by ID
